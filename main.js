e86c0fcf2292ae3c26e309fd98167a5e6d193f49
Abdool-Zaid <aztoorabally7447@gmail.com>
2023-04-06 09:52:34 +0200

    'object_maker'

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('working')
+function new_item() {
+    let key = document.querySelector('#key').value;
+    let value = document.querySelector('#value').value;
+    console.log(key, '|', value)
+    let obj = {}
+    obj[key] = value
+    console.log(obj)
+  }
+document.querySelector('#submit').addEventListener('click',new_item)
